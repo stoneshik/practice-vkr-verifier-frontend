4c76fe7a15c5282493ba6d1e0c36b75de1ede673
stoneshik <strebilly@mail.ru>
2026-04-03 15:56:33 +0300

    поменял порт бэкенда на 3351

--- app/utils/lib/axios.ts
+++ app/utils/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:8080/api/v1"
+export const baseURL = "http://localhost:3351/api/v1"
 
 export const api = axios.create({
     baseURL: baseURL,
